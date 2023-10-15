de2e9a43f1739a4a4514fc1eb104240393343668
Volodymyr Pavlov <vladimir.pavlov.mail@gmail.com>
2023-10-15 21:04:24 +0200

    nothing

--- sandboxes/sb2/main.cpp
+++ sandboxes/sb2/main.cpp
@@ -1,18 +1,7 @@
-#include <log4cxx/logger.h>
-#include <log4cxx/propertyconfigurator.h>
+#include <iostream>
 
-int main() {
-    // Initialize Log4cxx using your configuration file
-    log4cxx::PropertyConfigurator::configure("config/log4cxx.properties");
+int main(){
 
-    // Create a logger
-    log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("MyLogger"));
-
-    // Log messages at different levels
-    LOG4CXX_DEBUG(logger, "This is a DEBUG message");
-    LOG4CXX_INFO(logger, "This is an INFO message");
-    LOG4CXX_WARN(logger, "This is a WARN message");
-    LOG4CXX_ERROR(logger, "This is an ERROR message");
-
+    std::cout << "Hi\n";
     return 0;
 }
