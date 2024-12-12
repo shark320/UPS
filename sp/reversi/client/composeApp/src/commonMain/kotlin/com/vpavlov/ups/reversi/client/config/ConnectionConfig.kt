49e9f57a6245aa023201c4fbffecbb204a67d340
shark32 <shar32@zcu.cz>
2024-12-12 15:15:04 +0100

    [CLIENT] configuration validtion

--- sp/reversi/client/composeApp/src/commonMain/kotlin/com/vpavlov/ups/reversi/client/config/ConnectionConfig.kt
+++ sp/reversi/client/composeApp/src/commonMain/kotlin/com/vpavlov/ups/reversi/client/config/ConnectionConfig.kt
@@ -10,14 +10,23 @@ class ConnectionConfig(prop: Properties): Configuration(prop = prop) {
     val identifier: String
 
     val pingInterval: Long
-
+ 
     val isPing: Boolean
 
     init{
         port = get("port")!!.toInt()
+        require (port in 0..65535){
+            throw IllegalArgumentException("Invalid port configuration.")
+        }
         ip = get("ip")!!
         identifier = get("identifier")!!
+        require (identifier.length == 4){
+            throw IllegalArgumentException("Invalid identifier configuration.")
+        }
         pingInterval = get("ping_interval")!!.toLong()
+        require (pingInterval >=  0) {
+            throw IllegalArgumentException("Invalid ping interval configuration.")
+        }
         isPing = get("ping")!!.toBoolean()
     }
 }
