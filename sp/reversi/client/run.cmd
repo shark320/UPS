2ce43afffc4f64e74a1531890cfd9e1a1079aeb2
shark32 <shar32@zcu.cz>
2024-12-13 10:05:44 +0100

    [CLIENT] run cmd upd

--- sp/reversi/client/run.cmd
+++ sp/reversi/client/run.cmd
@@ -1,2 +1,2 @@
 set JAVA_HOME=C:\DEV_HOME\TOOLS\java\21.0.2
-gradlew desktopRun -DmainClass="com.vpavlov.ups.reversi.client.MainKt" --quiet
+gradlew desktopRun -DmainClass="com.vpavlov.ups.reversi.client.MainKt" --quiet --args="--ide"
